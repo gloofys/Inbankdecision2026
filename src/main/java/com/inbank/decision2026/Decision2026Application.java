2c51fca5c6128cfa2fb6b2514ca622140c40e892
gloofys <fred.brosman06@gmail.com>
2026-03-24 20:44:45 +0200

    Initial commit

---
+++ src/main/java/com/inbank/decision2026/Decision2026Application.java
@@ -0,0 +1,13 @@
+package com.inbank.decision2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Decision2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Decision2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/inbank/decision2026/Decision2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inbank.decision2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Decision2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
